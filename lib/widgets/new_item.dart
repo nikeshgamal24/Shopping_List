3e6f624f3683ed34601f24f02d3fd0249b58dd5b
nikeshgamal24 <nikesh.gamal24@gmail.com>
2023-06-15 13:26:36 +0545

    extracted the input values of the form

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItem extends StatefulWidget{
   const NewItem({super.key});
@@ -17,11 +18,21 @@ class NewItem extends StatefulWidget{
 class _NewItemState extends State<NewItem>{
   //to execute the validator we use GlobalKey() to check the currentState the will be used to automatically reach out the form field and execute the validator
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _enteredCategory = categories[Categories.vegetables]!;  
+
 
 
   void _saveItem(){
     //need to use the Global key to invoke the validator
-    _formKey.currentState!.validate();
+    if(_formKey.currentState!.validate()){
+      _formKey.currentState!.save();
+      //the .save() will trigger a speacial function on each and every text field of the form. The property name is 'onSaved'
+      print(_enteredName);
+      print(_enteredQuantity);
+      print(_enteredCategory);
+    }
   }
 
   @override
@@ -47,6 +58,10 @@ class _NewItemState extends State<NewItem>{
                     }
                     return null;
                   },
+                  // the value will be the entered value of the user to the form of the particular section 
+                  onSaved: (value){
+                    _enteredName = value!;
+                  },
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
@@ -65,11 +80,17 @@ class _NewItemState extends State<NewItem>{
                           }
                           return null;
                         },
+                        onSaved: (value){
+                          // parse is used it the value is wrong then it will return error
+                          // whereas if we use tryParse() then it will return null if the value is not the true 
+                          _enteredQuantity = int.parse(value!);
+                        },
                       ),
                     ),
                     const SizedBox(width: 8,),
                     Expanded(
                       child: DropdownButtonFormField(
+                        value: _enteredCategory,
                         items: [
                           for (final category in categories.entries)
                             DropdownMenuItem(
@@ -88,7 +109,9 @@ class _NewItemState extends State<NewItem>{
                             )
                         ], 
                         onChanged: (value){
-
+                          setState(() {
+                            _enteredCategory = value!;
+                          });
                         },
                         ),
                       ),
